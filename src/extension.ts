a8274652dac85341ff352b1283015e02642ddeeb
Y4NN777 <axel.studiesmail@gmail.com>
2025-09-16 03:31:37 +0000

    Updated the generateCommentCommand function to integrate the promptBuilder and call the BuildPrompt function for test -- Status : Working fine

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { buildPrompt } from './promptBuilder';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "y7-local-commenter" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('y7-local-commenter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Y7-local-commenter!');
+	const generateCommentCommand = vscode.commands.registerCommand('y7-local-commenter.generateComment', async () => {
+		vscode.window.showInformationMessage('Generation code comment, please wait ... !');
+
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor === undefined) {
+			vscode.window.showErrorMessage('Failed to retrieve editor');
+			return;
+		}
+
+		const prompt = await buildPrompt(editor);
+		console.log('prompt', prompt);
+
+		if (prompt === undefined ) {
+			vscode.window.showErrorMessage('Failed to generate prompt');
+			return;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(generateCommentCommand);
 }
 
 // This method is called when your extension is deactivated
